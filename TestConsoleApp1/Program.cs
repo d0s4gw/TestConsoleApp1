d65b6e9b2d09e24a8546a0ce13670cd288359bb5
Michael Welsh <michael.j.welsh@gmail.com>
2017-03-11 20:30:19 -0800

    Updating from hello world to fizz buzz

--- TestConsoleApp1/Program.cs
+++ TestConsoleApp1/Program.cs
@@ -4,9 +4,32 @@ namespace TestConsoleApp1
 {
     class Program
     {
+        public void run()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+               if (i % 15 == 0)
+                {
+                    Console.Out.WriteLine("FizzBuzz");
+                } else if (i % 5 == 0)
+                {
+                    Console.Out.WriteLine("Buzz");
+                } else if (i % 3 == 0)
+                {
+                    Console.Out.WriteLine("Fizz");
+                } else
+                {
+                    Console.Out.WriteLine(i);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting application...");
+            Program p = new Program();
+            p.run();
+            Console.WriteLine("Done!");
+            Console.ReadLine();
         }
     }
 }
